7904c8af52a2dfe81c8476f8efc700ecfe8d2f3f
Robin Tran <duytran.it@gmail.com>
2014-08-02 21:05:15 +0700

    re-added project

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Project do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :title, :share, :status, :description, :price
+  # permit_params :list, :of, :attributes, :on, :model
   #
   # or
   #
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :procategory
+
+
 	has_many :attachments, dependent: :destroy
 end
