83bdd110a9d8f974622f787a83f9060c926d3de6
Robin Tran <duytran.it@gmail.com>
2014-08-11 10:48:49 +0700

    staff can only show own projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.order("title").page(params[:page]).per(2)
+    @projects = current_user.projects.order("title").page(params[:page]).per(2)
   end
 
   # GET /projects/1
