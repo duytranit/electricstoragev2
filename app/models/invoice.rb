a01d3c4705363bafd04df22318be8d54fecc5dd3
Robin Tran <duytran.it@gmail.com>
2014-08-12 13:41:52 +0700

    validate from_date and end_date of invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,20 @@ class Invoice < ActiveRecord::Base
 	has_many :projects, through: :invoicedetails
 
 	belongs_to :user
+
+	validate :check_from_date?
+	validate :check_end_date?
+
+	private
+	def check_from_date?
+		if self.from_date < Date.today
+			errors.add(:from_date, 'is past')
+		end
+	end
+
+	def check_end_date?
+		if self.end_date < self.from_date
+			errors.add(:end_date, 'must over From date')
+		end
+	end
 end
