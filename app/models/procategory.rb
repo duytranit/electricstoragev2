b1cdbb75cc0be88527d0972269d83f63fd032ddf
Robin Tran <duytran.it@gmail.com>
2014-08-21 10:41:59 +0700

    remove un-used validation of procategory

--- app/models/procategory.rb
+++ app/models/procategory.rb
@@ -5,6 +5,16 @@ class Procategory < ActiveRecord::Base
 	has_many :projects
 	has_many :procategories
 
+	validate :check_name?
+
+	def check_name?
+		name = self.name
+
+		if (name[0..1] == '') || (name[0..1] == ' ')
+			errors.add(:name, 'cannot start with a space')
+		end
+	end
+
 	def full_name
     flag = self.level.to_i
     name = self.ddc + " - "
