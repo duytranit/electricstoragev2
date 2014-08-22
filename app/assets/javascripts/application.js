b2c02ca0d7cad8886d0cb84102ac0a3bcf0b70f8
Robin Tran <duytran.it@gmail.com>
2014-08-22 23:59:28 +0700

    wmd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require wmd
 //= require showdown
 //= require jquery-1.2.6
 //= require jquery-1.2.6.min
