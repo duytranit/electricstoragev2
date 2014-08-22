5623b61ffe9c6a396381a437c32d97bc83888401
Robin Tran <duytran.it@gmail.com>
2014-08-22 08:00:25 +0700

    fix error when search with empty min price and max price

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-
   	if params[:title] || params[:description] || params[:procategory_id]
   		@projects = Project.search(params[:title], params[:description], params[:min_price], params[:max_price], params[:procategory_id]).joins(:procategory).where(["projects.share = ? and projects.status = ? 
   		and procategories.status = ? ", true, true, true]).page(params[:page]).per(2)
   	else
   		@projects = Project.joins(:procategory).where(["projects.share = ? and projects.status = ? and procategories.status = ? ", true, true, true]).page(params[:page]).per(2)
   	end
-
-  	
   end
 end
