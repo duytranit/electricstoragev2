3feb5dd63ecf2f3dbdd272932b4ccc32b53dddcd
Robin Tran <duytran.it@gmail.com>
2014-08-26 16:53:06 +0700

    add more procategory to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,6 +43,180 @@ procategory = Procategory.create(
 	level: 0
 	)
 
+congnghe = procategory.procategories.create(
+	name: "Công nghệ",
+	status: true,
+	created_at: DateTime.now,
+	updated_at: DateTime.now,
+	user_id: staff.id,
+	ddc: "600",
+	level: 1,
+	description: "<p><em>Xem thêm 303.48 về công nghệ như là nguyên nhân của sự thay đổi văn hóa; cũng xem 306.4 về xã hội học của công nghệ; cũng xem 338.1-338.4 về khía cạnh kinh tế của công nghiệp dựa trên các công nghệ cụ thể; cũng xem 338.9 về công nghệ thích hợp; chuyển giao công nghệ.<em></p>
+
+							<p><em>Xem Phần hướng dẫn ở 300 so với 600; cũng xem ở 363 so với 302-307, 333.7, 570-590, 600; vũng xem ở 363.1 so với 600; cũng xem ở 583-585 so với 600</em></p>
+
+							<strong><p>TÓM LƯỢT</p>
+
+							<p>601-609 : Tiểu phân mục chung và vẽ kỹ thuật, công nghệ vật liệu nguy hiểm</p>
+
+							<p>610: Y học và sức khỏe</p>
+
+							<p>620: Kỹ thuật và các hoạt dộng liên quan</p>
+
+							<p>630: Nông nghiệp và các công nghệ liên quan</p>
+
+							<p>640: Quản lý nhà cửa và gia đình</p>
+
+							<p>650: Quản lý và các dịch vụ phụ trợ</p>
+
+							<p>660: Kỹ thuật hóa học và các  công nghệ liên quan</p>
+
+							<p>670: Công nghệ sản xuất</p>
+
+							<p>680: Sản xuất sản phẩm chuyên dựng</p>
+
+							<p>690: Nhà &amp; Xây dựng</p><strong>"
+	)
+
+nongnghiep = congnghe.procategories.create(
+	name: "Nông nghiệp và các công nghệ liên quan",
+	status: true,
+	created_at: DateTime.now,
+	updated_at: DateTime.now,
+	user_id: staff.id,
+	ddc: "630",
+	level: congnghe.level + 1,
+	description: "<p>Tiểu phân mục chung được thêm vào cho chung nông nghiệp và các công nghệ liên quan, cho riêng nông nghiệp.</p>
+							<p>Xếp vào đây tác phẩm liên ngành về tầm quan trọng của nông nghiệp [trước đây cũng là 580]; trồng trọt, trang trại, cây trồng.</p>
+							<p>Xếp xã hội học nông nghiệp vào 306.3; xếp kinh tế nông nghiệp vào 338.1</p>
+							<em><p>Xem thêm 307.72 về xã hội học nông thôn; cũng xem 333.76 về kinh tế đất nông nghiệp; cũng xem 909 về tác phẩm tổng quát về điều kiện và văn minh nông thôn; cũng xem 930-990 về điều kiện và văn minh nông thôn các khu vực cụ thể.</p>
+							<p>Xem phần hướng dẫn ở 571-575 so với 630; cũng xem ở 630 so với 579-590, 641.3</p><em/>
+
+							<strong><p>TÓM LƯỢC</p>
+
+							<p>630.1-.9: Tiểu phân mục chung</p>
+							<p>631: Kỹ thuật cụ thể; máy móc, thiết bị, vật liệu</p>
+							<p>632: Xâm hại, bệnh, sâu hại cây trồng</p>
+							<p>633: Cây trồng ngoài đồng và trong đồn điền</p>
+							<p>634: Vười cây ăn quả, quả, lâm nghiệp</p>
+							<p>635: Cây trồng trong vườn (Nghề làm vườn) Rau</p>
+							<p>636: Chăn nuôi</p>
+							<p>637: Chế biến sữa và các sản phẩm liên quan</p>
+							<p>638: Nuôi côn trùng</p>
+							<p>639: Săn bắn, nghề cá, bảo tồn, các kỹ thuật liên quan</p></strong>"
+	)
+
+kythuat = nongnghiep.procategories.create(
+	name: "Kỹ thuật cụ thể; máy móc, thiết bị, vật liệu",
+	status: true,
+	created_at: DateTime.now,
+	updated_at: DateTime.now,
+	user_id: staff.id,
+	ddc: "631",
+	level: nongnghiep.level + 1,
+	description: "<p>Đề tài chung cho trồng trọt và chăn nuôi hoặc giới hạn cho trồng trọt</p>
+								<p>Xếp tác phẩm tổng hợp về máy móc, thiết bị, vật liệu sử dụng cho kỹ thuật hoặc quy trình phụ trợ cụ thể, vd., máy tính, vào 630.2</p>
+								<p><em>Về thiệt hại, bênh, sau hại cây trồng, xem 632; về kỹ thuật, máy móc, thiết bị, vật liệu cho các cây trồng cụ thể, xem 633-635; về kỹ thuật, máy móc, thiết bị, vật liệu cụ thể cho các ngành chăn nuôi, xem 636</em></p>"
+	)
+
+xaydung = kythuat.procategories.create(
+	name: "Công trình xây dựng trong nông nghiệp",
+	status: true,
+	created_at: DateTime.now,
+	updated_at: DateTime.now,
+	user_id: staff.id,
+	ddc: "631.2",
+	level: kythuat.level + 1,
+	description: "<p>Bao gồm cả kho chứa, đập, hàng rào; đường đi, cầu cống</p>
+								<p>Xếp xây dựng cầu vào 624.2; xếp xây dựng đường trong trang trại vào 625.7; xếp xây dựng đập vào 627; xếp xây dựng nhà và các kết cấu khác trong trang trại vào 690</p>
+								<p><em>Về làm chuồng cho gia súc, xem 636.08</em></p>"
+	)
+
+dungcu = kythuat.procategories.create(
+	name: "Dụng cụ, cơ khí, máy móc, thiết bị",
+	status: true,
+	created_at: DateTime.now,
+	updated_at: DateTime.now,
+	user_id: staff.id,
+	ddc: "631.3",
+	level: kythuat.level + 1,
+	description: "<p>Bao gồm nhà xưởng</p>
+								<p>Xếp chế tạo dụng cụ, cơ khí, máy móc, thiết bị vào 681. Xếp chế tạo một loại máy cụ thể theo loại đó, vd., chế tạo máy kéo 629.225</p>
+								<p><em>Về thiết bị cho một mục đích cụ thể, xem mục đích đó, vd., nhà kính 631.5</em></p>
+								<p><em>Xem thêm 631.2 về công trình xây dựng trong nông nghiệp</em></p>"
+	)
+
+khoahocdat = kythuat.procategories.create(
+	name: "Khoa học đất",
+	status: true,
+	created_at: DateTime.now,
+	updated_at: DateTime.now,
+	user_id: staff.id,
+	ddc: "631.4",
+	level: kythuat.level + 1,
+	description: "<p>Bao gồm cả xói mòn, lập bản đồ thổ nhưỡng, tác phẩm nông nghiệp tổng hợp về bảo tồn đất và nước</p>
+								<p>Xếp vào đây tác phẩm liên ngành về đất</p>
+								<p>Xếp sử dụng phân bón và các chất cải tạo đất vào 631.8; xếp tác phẩm kỹ thuật tổng hợp về xói mòn đất vào 627; xếp  tác phẩm liên ngành về bảo tồn đất vào 333.73; xếp tác phẩm liên ngành về xói mòn đất vào 551.3; xếp tác phẩm liên ngành về cơ học đất vào 624.1</p>
+								<p><em>Về phục hồi đất, xem 631.6; về bảo tồn nước, xem 631.7. Về một khía cạnh cụ thể của đất, xem khía cạnh đó, vd., thành tạo thổ nhưỡng 551.3, sử dụng đất trong xây dựng 624.1</em></p>"
+	)
+
+trongtrot = kythuat.procategories.create(
+	name: "Trồng trọt và thu hoạch",
+	status: true,
+	created_at: DateTime.now,
+	updated_at: DateTime.now,
+	user_id: staff.id,
+	ddc: "631.5",
+	level: kythuat.level + 1,
+	description: "<p>Bao gồm cả di truyền học nông nghiệp, vườn ươm, nhân giống cây trồng, hạt giống và danh muc hạt giống, các giống cây trồng, năng suất; các công đoạn sau thu hoạch, vd., phân loại, bảo quản trong kho; các phương pháp trồng trọt đặc biệt, vd., luân canh, trồng trọt khô, nông nghiệp nhà kính, tưới tiêu, trồng trọt hữu cơ, nông nghiệp không cần đất (thủy canh)</p>
+
+								<p>Chỉ sử dụng chỉ số này cho tưới tiêu khi tác phẩm mô tả liên quan tới công việc trong trang trại, vd., lắp đặt và sử dụng hệ thống tưới phun</p>
+
+								<p>Xếp các phương pháp trồng trọt đặc biệt như là đề tài trong kinh tế đất đai vào 333.76; xếp các phương pháp trồng trọt đặc biệt như là đề tài trong kinh tế nông nghiệp vào 338.1; xếp sinh họcc cây nông nghiệp vào 580; xếp đào giếng vào 628.1; xếp tác phẩm liên ngành về tưới tiêu vào 333.91; xếp tác phẩm liên ngành về khía cạnh kỹ thuật của tưới tiêu, các công rình nhận nước tưới từ nguồi ngoài trang trại vào 627</p>
+
+								<p><em>Về thoát nước thải, xem 628.3; về trồng trọt dể bảo tồn đất, luân canh cây trồng chống xói mòn, xem 631.4; về nhà kính, làm vườn hữu cơ, xem 635. Về từng khía cạnh của trồng trọt hữu cơ, xem khía cạnh đó, vd., phân vi sinh 631.8</em></p>
+
+								<p><em>Xem phần hướng dẫn ở 338.1 so với 631.5</em></p>"
+	)
+
+dondat = kythuat.procategories.create(
+	name: "Dọn đất, tiêu nước, phục hồi đất",
+	status: true,
+	created_at: DateTime.now,
+	updated_at: DateTime.now,
+	user_id: staff.id,
+	ddc: "631.6",
+	level: kythuat.level + 1,
+	description: "<p>Bao gồm cả cải tạo bề mặt đất mỏ</p>
+								<p>Xếp vào đây cải tạo đất</p>
+								<p>Xếp phục hồi rừng vào 634.9; xếp các dự án tiêu nước ngoài trang trại, tác phẩm liên ngành về các khía cạnh kỹ thuật của tiêu nước và cải tạo đất vào 627</p>"
+	)
+
+baotonnuoc = kythuat.procategories.create(
+	name: "Bảo tồn nước",
+	status: true,
+	created_at: DateTime.now,
+	updated_at: DateTime.now,
+	user_id: staff.id,
+	ddc: "631.7",
+	level: kythuat.level + 1,
+	description: "<p><em>Về trồng trọt để bảo tồn nước, xem 631.5</em></p>"
+	)
+
+phanbon = kythuat.procategories.create(
+	name: "Phân bón, chất cải tạo đất, chất điều hòa sinh trưởng",
+	status: true,
+	created_at: DateTime.now,
+	updated_at: DateTime.now,
+	user_id: staff.id,
+	ddc: "631.8",
+	level: kythuat.level + 1,
+	description: "<p>Tiểu phân mục chung được thêm vào cho chung phân bón, chất dưỡng đất, chất điều hòa sinh trưởng, cho riêng phân bón</p>
+								<p>Xếp vào đây tác phẩm liên ngành về hóa chất nông nghiệp</p>
+								<p>Xếp tác phẩm tổng hợp về độ phì đất vào 631.4</p>
+								<p><em>Về thuốc trừ sâu, xem 632; về sản xuất hóa chất nông nghiệp, xem 668</em></p>"
+	)
+
 cn = procategory.procategories.create(
 	name: 'Chăn Nuôi',
 	status: true,
